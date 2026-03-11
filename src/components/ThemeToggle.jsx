4b7ad3075d9b7f8cc94cb9a167c0de7c2c34b2b8
saranya <saranyavanamutham@gmail.com>
2026-03-11 11:55:55 +0530

    Change default theme mode to light

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -4,7 +4,7 @@ import { cn } from '@/lib/utils';
 
 export const ThemeToggle = () => {
 
-    const[isDarkMode,setIsDarkMode]=useState(true);
+    const[isDarkMode,setIsDarkMode]=useState(false);
 
     useEffect(()=>{
 const storedTheme = localStorage.getItem("theme")
